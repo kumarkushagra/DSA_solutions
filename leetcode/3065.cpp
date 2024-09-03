11ada3e19b3c9384431a8a493b4822124cbb4688
kumarkushagra <kumarkushagra777@gmail.com>
2024-09-03 23:27:04 +0530

    maintaining github streal (DSA solution)

---
+++ leetcode/3065.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
+    int sum = 0;
+    int r=0, c= 0;
+    while(r<=grid.size()){
+        while(c<=grid[0].size()){
+            1;
+        }
+    }
+
+}
+
+int main(){
+    vector<vector<int>> grid = {{3,0,8,4},{2,4,5,7},{9,2,6,3},{0,3,1,0}};
+
+    return 0;
+}
